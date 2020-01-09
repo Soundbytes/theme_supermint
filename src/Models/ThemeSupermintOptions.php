c97ca8df132063754140b0bbc9786450d319b2a2
Andreas Sumerauer <ansum@soundbytes.de>
2020-01-09 15:03:59 +0100

    another php7.3 compatability issue fixed

--- src/Models/ThemeSupermintOptions.php
+++ src/Models/ThemeSupermintOptions.php
@@ -222,6 +222,15 @@ class ThemeSupermintOptions extends ConcreteObject
 		return $exportDOM->saveHTML();
 	}
 
+	function safecount($val){
+        if (is_array($val)) {
+			return count($val);
+		}
+		else {
+			return false;
+		}
+	}
+
 	function importXML_preset ($file, $pID = false, $updateOnly = false) {
 
 		$content = file_get_contents($file);
@@ -232,7 +241,7 @@ class ThemeSupermintOptions extends ConcreteObject
 		// On tyest si on a un tableau et qu'il n'est pas vide
 		if (is_array($p) && count($p)) :
 			// On teste les different conteneurs
-			if(count($p['mcl_preset']) && count($p['mcl_preset']['config'] && count($p['mcl_preset']['options']))) :
+			if($this->safecount($p['mcl_preset']) && $this->safecount($p['mcl_preset']['config'] && $this->safecount($p['mcl_preset']['options']))) :
 				$pp = $p['mcl_preset'];
 				if ($this->pkg->getPackageHandle() != $pp['config']['theme']) return array ('error' => true, 'message' => t('This preset in not compatible with this theme'));
 				if (!$pID) :
